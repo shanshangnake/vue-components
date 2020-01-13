0b188b3b3634bf253fc8064e87871d4c57e2f7dd
吴春玲 <wuchunling@C02W26Z9G8WN.local>
2020-01-13 18:43:14 +0800

    header component

---
+++ VueDemo/App.vue
@@ -0,0 +1,10 @@
+<template>
+  <h1>hello</h1>  
+</template>
+<script>
+export default {
+  monunted() {
+    // console.info('hello');
+  }
+}
+</script>
---
+++ VueDemo/components/Header.vue
@@ -0,0 +1,70 @@
+<template>
+<div>
+  <nav>
+    <a href="" class="home">Home</a>
+    <a href="" class="projects">Projects</a>
+    <a href="" class="services">Services</a>
+  </nav>
+  <p>选择了<span>{{active}}菜单</span></p>
+</div>
+</template>
+<script>
+
+</script>
+
+<style scoped>
+a, a:visited {
+  outline: none;
+  color: #389dc1;
+}
+a:hover {
+  text-decoration: none;
+}
+section, footer, header, aside, nav {
+  display: block
+}
+
+nav {
+  display: inline-block;
+  margin: 60px auto 45px;
+  background-color: #5597b4;
+  box-shadow: 0 1px 1px #ccc;
+  border-radius: 2px;
+}
+
+nav a {
+  display: inline-block;
+  padding: 18px 30px;
+  color: #fff !important;
+  font-weight: bold;
+  font-size: 16px;
+  text-decoration: none !important;
+  line-height: 1;
+  text-transform: uppercase;
+  background-color: transparent;
+  transform: background-color 0.25s;
+}
+nav a:first-child {
+  border-radius: 2px 0 0 2px;
+}
+nav a:last-child {
+  border-radius: 02px 2px 0;
+}
+nav.home .home, nav.projects .projects, nav.services .services {
+  background-color: #e35885;
+}
+p {
+  font-size: 22px;
+  font-weight: bold;
+  color: #7d9098;
+}
+p span {
+  color:#ffffff;
+  display:inline-block;
+  padding:5px 10px;
+  background-color:#c4d7e0;
+  border-radius:2px;
+  text-transform:uppercase;
+  font-size:18px;
+}
+</style>
